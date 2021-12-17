8480376ac9f142aafde877f6546dcd5a796e785b
mellabanca <91677889+mellabanca@users.noreply.github.com>
2021-12-16 21:26:57 -0300

    16/12/2021

---
+++ js/Canhao.js
@@ -0,0 +1,9 @@
+class Canhao {
+    constructor(x, y, lar, alt, angulo){
+        this.x = x;
+        this.y = y;
+        this.lar = lar;
+        this.alt = alt;
+        this.angulo = angulo;
+    }
+}
