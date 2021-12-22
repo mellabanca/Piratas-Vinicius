0077803f7254119b0077bca4342b4f59048f2244
mellabanca <91677889+mellabanca@users.noreply.github.com>
2021-12-21 21:08:12 -0300

    21/12

--- js/Canhao.js
+++ js/Canhao.js
@@ -5,5 +5,21 @@ class Canhao {
         this.lar = lar;
         this.alt = alt;
         this.angulo = angulo;
+        this.base = loadImage("./assets/cannonBase.png");
+        this.cano = loadImage("./assets/canon.png");
+    }
+    display(){
+        push();
+        imageMode(CENTER);
+        image(this.cano,this.x,this.y,this.lar,this.alt);
+        
+
+
+
+        pop();
+
+        image(this.base,70,20,200,200);
+        noFill();
+
     }
 }
